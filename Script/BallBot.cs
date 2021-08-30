25f83242960bed939064132d2023a693e2f6627d
0Plus0YiLong <40749457+0Plus0YiLong@users.noreply.github.com>
2021-08-30 20:34:13 +1200

    Add files via upload

---
+++ Script/BallBot.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallBot : MonoBehaviour
+{
+    public Transform botpos;
+    public GameObject ballprefab;
+    public float timer;
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > 1f)
+        {
+            Instantiate(ballprefab, botpos.position, transform.rotation);
+            timer = 0;
+        }
+    }
+}
---
+++ Script/BallMovement.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+    public float speed = 20;
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.velocity = transform.forward * speed;
+    }
+}
